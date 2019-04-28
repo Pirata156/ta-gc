54353ccf5c2d6b27ea25c0b93d4602f2ba2bfb89
Pirata156 <Pirata156@users.noreply.github.com>
2019-04-28 11:54:54 +0100

    WebODM results

---
+++ results/potree_pointcloud/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "points": 10717212,
+    "boundingBox": {
+        "lx": 554034.7,
+        "ly": 4505755.04,
+        "lz": -18.53,
+        "ux": 554445.06,
+        "uy": 4506165.4,
+        "uz": 391.8300000001025
+    },
+    "tightBoundingBox": {
+        "lx": 554034.7,
+        "ly": 4505755.04,
+        "lz": -18.53,
+        "ux": 554445.06,
+        "uy": 4506089.29,
+        "uz": 63.56
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 2.84305739402771,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
